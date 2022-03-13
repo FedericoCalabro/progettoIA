0015f0e4c3ac6428322307a382f925e9cc001791
Alfredo <alfredo.bressi.work@gmail.com>
2022-03-13 12:08:05 +0100

    aggiunta stampa nel callback

--- src/main/java/coloruid/dlv/DlvCallback.java
+++ src/main/java/coloruid/dlv/DlvCallback.java
@@ -16,7 +16,6 @@ public class DlvCallback implements Callback {
         GraphPanel.getInstance().getTextArea().append("Execution finished!"+ System.lineSeparator());
         AnswerSets answersets = (AnswerSets) output;
         List<String> temp = new ArrayList<String>();
-
         for(AnswerSet a:answersets.getOptimalAnswerSets()){
             try {
                 for(Object obj:a.getValue()){
@@ -27,16 +26,20 @@ public class DlvCallback implements Callback {
             }
 
         }
-        temp.sort(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                Integer s1 = Integer.parseInt(o1.split(",")[2].substring(0,o1.split(",")[2].length()-1));
-                Integer s2 = Integer.parseInt(o2.split(",")[2].substring(0,o2.split(",")[2].length()-1));
-                return s1.compareTo(s2);
+        if(temp.isEmpty()){
+            GraphPanel.getInstance().getTextArea().append("No solution found!" + System.lineSeparator());
+        }else {
+            temp.sort(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    Integer s1 = Integer.parseInt(o1.split(",")[2].substring(0, o1.split(",")[2].length() - 1));
+                    Integer s2 = Integer.parseInt(o2.split(",")[2].substring(0, o2.split(",")[2].length() - 1));
+                    return s1.compareTo(s2);
+                }
+            });
+            for (String s : temp) {
+                GraphPanel.getInstance().getTextArea().append(s + System.lineSeparator());
             }
-        });
-        for(String s: temp){
-            GraphPanel.getInstance().getTextArea().append(s + System.lineSeparator());
         }
     }
 }
